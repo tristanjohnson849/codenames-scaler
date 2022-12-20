d43fd935156364f8621c6d7724be5c66464427f5
Tristan Johnson <tristan.johnson849@gmail.com>
2022-12-20 12:17:44 -0800

    Fix card widths and margins

--- src/BoardForm/InputCard.tsx
+++ src/BoardForm/InputCard.tsx
@@ -8,7 +8,7 @@ export interface InputCardProps {
 
 export const InputCard: React.FC<InputCardProps> = ({ title, style = {}, children }) => {
     return (
-        <div style={{ marginLeft: '24px', maxWidth: '33%', ...style }}>
+        <div style={{ marginLeft: '24px', marginBottom: '24px', maxWidth: '33%', minWidth: '180px', ...style }}>
             <div style={{ paddingLeft: '16px', paddingBottom: '8px' }}><b>{title}</b></div>
             <div style={{
                 border: '1px solid #555',
